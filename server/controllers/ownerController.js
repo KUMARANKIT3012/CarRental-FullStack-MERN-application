ad61047b69de91b5aeea321c3e05768118efe571
KUMARANKIT3012 <ankitkumar30122004@gmail.com>
2026-03-07 21:16:44 +0530

    update owner controller

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -3,7 +3,7 @@
 
 import imagekit from '../configs/imageKit.js';
 import Booking from '../models/Booking.js';
-import Car from '../models/car.js';
+import Car from '../models/Car.js';
 import User from '../models/User.js';
 import fs from 'fs';
 
