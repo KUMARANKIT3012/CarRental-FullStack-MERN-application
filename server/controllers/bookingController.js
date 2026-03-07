1dea795bb98452676773cccfb6cf7e73a3b4a6d2
KUMARANKIT3012 <ankitkumar30122004@gmail.com>
2026-03-07 23:47:56 +0530

    fix: resolve case-sensitive import for Car model

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,5 @@
 import Booking from "../models/Booking.js";
-import Car from "../models/car.js";
+import Car from "../models/Car.js";
 
 // Function to Check Availability of Car for a given Date
 const checkAvailability = async (car, pickupDate, returnDate) => {
